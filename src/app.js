2e602cb19b991c44f719c79a2e0ff3a5fb50dd88
dedahiahmed <dedahiahmed10@gmail.com>
2024-06-03 02:37:52 +0000

    initial commit

---
+++ src/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const ejs = require("ejs"); // Import EJS module
+const app = express();
+
+// Set EJS as the view engine
+app.set("view engine", "ejs");
+
+// Set the views directory
+app.set("views", path.join(__dirname, "views"));
+
+// Serve static files from the "public" directory
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+// Your existing routes and middleware
+app.get("/", (req, res) => {
+  const navbarContent = fs.readFileSync(
+    path.join(__dirname, "/components/navbar.ejs"),
+    "utf8"
+  );
+  const sidebarContent = fs.readFileSync(
+    path.join(__dirname, "/components/sidebar.ejs"),
+    "utf8"
+  );
+
+  res.render("index", {
+    title: "Home",
+    navbar: navbarContent,
+    sidebar: sidebarContent,
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
---
+++ src/routes/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const router = express.Router();
+
+// Helper function to read component files
+const readComponent = (filePath) => {
+  return fs.readFileSync(
+    path.join(__dirname, "..", "components", filePath),
+    "utf8"
+  );
+};
+
+router.get("/", (req, res) => {
+  try {
+    // Read components
+    const navbar = readComponent("Navbar.ejs");
+    const sidebar = readComponent("Sidebar.ejs");
+
+    res.render("index", { title: "Home", navbar, sidebar });
+  } catch (error) {
+    console.error("Error reading components:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+module.exports = router;
