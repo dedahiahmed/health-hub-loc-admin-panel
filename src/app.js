d71dedac1ecdb0209ed5992d22ada9e7a7b5eccc
dedahiahmed <dedahiahmed10@gmail.com>
2024-06-14 21:22:16 +0000

    crypt the token

--- src/app.js
+++ src/app.js
@@ -2,8 +2,31 @@ const express = require("express");
 const path = require("path");
 const ejs = require("ejs");
 const cookieParser = require("cookie-parser");
+const crypto = require("crypto");
+const dotenv = require("dotenv");
 const app = express();
 
+dotenv.config();
+
+const encryptionKey = process.env.ENCRYPTION_KEY;
+const algorithm = process.env.ENCRYPTION_ALGORITHM;
+function encryptToken(token) {
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv(algorithm, encryptionKey, iv);
+  let encryptedToken = cipher.update(token, "utf8", "hex");
+  encryptedToken += cipher.final("hex");
+  return `${iv.toString("hex")}:${encryptedToken}`;
+}
+
+// Function to decrypt the token
+function decryptToken(encryptedToken) {
+  const [ivHex, tokenHex] = encryptedToken.split(":");
+  const iv = Buffer.from(ivHex, "hex");
+  const decipher = crypto.createDecipheriv(algorithm, encryptionKey, iv);
+  let decryptedToken = decipher.update(tokenHex, "hex", "utf8");
+  decryptedToken += decipher.final("utf8");
+  return decryptedToken;
+}
 // Set EJS as the view engine
 app.set("view engine", "ejs");
 // Set the views directory
@@ -46,7 +69,9 @@ app.post("/api/login", async (req, res) => {
     if (response.ok) {
       const data = await response.json();
       const token = data.token;
-      res.cookie("token", token, { httpOnly: true });
+      const encryptedToken = encryptToken(token);
+
+      res.cookie("token", encryptedToken, { httpOnly: true });
       res.json({ success: true });
     } else {
       const errorData = await response.json();
@@ -58,14 +83,16 @@ app.post("/api/login", async (req, res) => {
   }
 });
 const checkToken = async (req, res, next) => {
-  const token = req.cookies.token;
+  const encryptedToken = req.cookies.token;
 
-  if (!token) {
+  if (!encryptedToken) {
     // If token is not found, redirect to login page
     return res.redirect("/login");
   }
 
   try {
+    const token = decryptToken(encryptedToken);
+    console.log("token", token);
     const response = await fetch("http://localhost:8080/api/auth/user-me", {
       method: "GET",
       headers: {
